e47b10be0f99bff1249ed2dbacb30c5ee4cc2cc7
PacoCS <pacocayuela13@gmail.com>
2020-02-21 19:27:06 +0100

    21022020_1927

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>CORONALINUX</title>
+  </head>
+  <body>
+
+    <h1>CoronaLinux TEST</h1>
+    <h2>TEXT </h2>
+
+    <?php
+      phpinfo()
+    ?>
+
+  </body>
+</html>
