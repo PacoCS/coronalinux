f40da21241d2ad67db44b6ec31fe05980aad5784
PacoCS <pacocayuela13@gmail.com>
2020-02-28 19:34:45 +0100

    conn4

--- web/index.php
+++ web/index.php
@@ -9,7 +9,7 @@
     <h1>CoronaLinux TEST</h1>
     <h2>TEXT </h2>
 
-    <form action="index.html" method="post">
+    <form action="index.php" method="post">
       <input type="text" name="usuari">
       <br>
       <input type="text" name="contrasenya">
